bb993e594ccab5fa5d249e08b9b394ab066c3382
k1ama <ianpeterkiama@gmail.com>
2025-02-20 23:31:28 +0300

    add static directory listing disabling

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
 	fileserver := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
+	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileserver)))
 
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
@@ -18,4 +19,14 @@ func main() {
 	log.Print("starting servcer on: 4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+}
+
+func neuter(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasSuffix(r.URL.Path, "/") {
+			http.NotFound(w, r)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
