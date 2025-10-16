44e6c7f6c7f3d61c8fee857e5e2f5a827aad2330
k1ama <ianpeterkiama@gmail.com>
2025-10-16 12:45:59 +0300

    feat: add TLS self signed cert for secure HTTP requests

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -71,7 +71,7 @@ func main() {
 	}
 
 	infoLog.Printf("starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 }
 
